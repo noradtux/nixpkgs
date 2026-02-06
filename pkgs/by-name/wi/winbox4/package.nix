4fd66df518aa4215ef21b0ceb72ab7a3763c1745
Malte Schröder <malte.schroeder@tnxip.de>
2026-02-06 12:44:09 +0100

    Update WinBox4 to 4.0rc1

--- pkgs/by-name/wi/winbox4/package.nix
+++ pkgs/by-name/wi/winbox4/package.nix
@@ -5,7 +5,7 @@
 }:
 let
   pname = "winbox";
-  version = "4.0beta44";
+  version = "4.0rc1";
 
   metaCommon = {
     description = "Graphical configuration utility for RouterOS-based devices";
@@ -23,13 +23,13 @@ let
   x86_64-zip = callPackage ./build-from-zip.nix {
     inherit pname version metaCommon;
 
-    hash = "sha256-LPq7KPOj59NUhoQCxpAVW8qbjXJGxRw8fRJT7/qDtZM=";
+    hash = "sha256-Ko8+3zLILlUIl4VI7xqBWQgmL5CELgSTyjYgSAuFPWw=";
   };
 
   x86_64-dmg = callPackage ./build-from-dmg.nix {
     inherit pname version metaCommon;
 
-    hash = "sha256-96lb8a70dmqieKn5Nr61sZg/aVDLz0sY64sfN83rU+0=";
+    hash = "sha256-6qvhOouSsvcZwOZz5zNHMXCuc6yh8diKzytalkQmzAA=";
   };
 in
 (if stdenvNoCC.hostPlatform.isDarwin then x86_64-dmg else x86_64-zip).overrideAttrs (oldAttrs: {
